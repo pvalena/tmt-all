bf6beb444631c3bc137f4cc90263c7c0de29b6ed
Pavel Valena <pvalena@redhat.com>
2020-06-17 13:31:43 +0200

    Fixup tests/plans and Dockerfiles.

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ bash -n "$0"
 ls -d Dockerfile.${1:-*} | cut -d'.' -f2- \
   | xargs -i bash -c "
       im='pvalena/fedora-tmt-all:{}'
-      echo podman rmi -f \$im && \
-      echo podman build -f Dockerfile.{} . -t \$im && \
-      echo podman run --rm -it \$im \
+      podman rmi -f \$im
+      podman build -f Dockerfile.{} . -t \$im && \
+      podman run --rm -it \$im \
       || exit 255
   "
