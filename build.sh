68020809c6087234b554bcacac885ce5c9c6f354
Pavel Valena <pvalena@redhat.com>
2020-06-22 17:13:11 +0200

    Disable build test, add passwordless sudo.

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ bash -n "$0"
 ls -d Dockerfile.${1:-*} | cut -d'.' -f2- \
   | xargs -i bash -c "
       im='pvalena/fedora-tmt-all:{}'
+      set -x
+      podman pull 'fedora:{}'
       podman rmi -f \$im
       podman build -f Dockerfile.{} . -t \$im && \
-      podman run --rm -it \$im && \
       podman push \$im \
       || exit 255
   "
+      #podman run --rm -it \$im && \
